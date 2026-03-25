canopybmc/canopybmc

commit 880fc06ec3a081f06c87c07eade43210749511df

meta-hpe: add host power management with x86-power-control
Add x86-power-control recipe with GPIO signal mapping for the GXP
power controller (PowerOk, PowerOut, ResetOut, PostComplete,
PowerButton). Configure machine features for phosphor chassis and
fan management, and set virtual runtime providers for host and
chassis state managers. Add chassis, fans, and flash packages to
packagegroup-hpe-apps.

Signed-off-by: Marcello Sylvester Bauer <marcello.bauer@9elements.com>

meta-canopy/meta-hpe/meta-common/recipes-x86/chassis/x86-power-control_%.bbappend
@@ -1,0 +1,10 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+SRC_URI += " \
+	file://power-config-host0.json \
+	"
+
+do_install:append() {
+	install -d ${D}/${datadir}/${PN}
+	install -m 0644 ${UNPACKDIR}/power-config-host0.json ${D}/${datadir}/${PN}
+}

meta-canopy/meta-hpe/meta-common/recipes-hpe/packagegroups/packagegroup-hpe-apps.bb
@@ -4,10 +4,37 @@ PR = "r1"
 inherit packagegroup
 
 PROVIDES = "${PACKAGES}"
-PACKAGES = "${PN}-system"
+PACKAGES = " \
+        ${PN}-chassis \
+        ${PN}-fans \
+        ${PN}-flash \
+        ${PN}-system \
+        "
 
+PROVIDES += "virtual/obmc-chassis-mgmt"
+PROVIDES += "virtual/obmc-fan-mgmt"
+PROVIDES += "virtual/obmc-flash-mgmt"
 PROVIDES += "virtual/obmc-system-mgmt"
+
+RPROVIDES:${PN}-chassis += "virtual-obmc-chassis-mgmt"
+RPROVIDES:${PN}-fans += "virtual-obmc-fan-mgmt"
+RPROVIDES:${PN}-flash += "virtual-obmc-flash-mgmt"
 RPROVIDES:${PN}-system += "virtual-obmc-system-mgmt"
+
+SUMMARY:${PN}-chassis = "HPE Chassis"
+RDEPENDS:${PN}-chassis = " \
+        x86-power-control \
+        "
+
+SUMMARY:${PN}-fans = "HPE Fans"
+RDEPENDS:${PN}-fans = " \
+        phosphor-pid-control \
+        "
+
+SUMMARY:${PN}-flash = "HPE Flash"
+RDEPENDS:${PN}-flash = " \
+        phosphor-software-manager \
+        "
 
 SUMMARY:${PN}-system = "HPE System"
 RDEPENDS:${PN}-system = " \

meta-canopy/meta-hpe/meta-gxp/conf/machine/include/gxp.inc
@@ -24,5 +24,20 @@ UBOOT_DTBO_LOADADDRESS ?= "0x48100000"
 
 SERIAL_CONSOLES ?= "115200;ttyS2"
 
-# HPE packagegroup provider
+MACHINE_FEATURES += "\
+    obmc-phosphor-chassis-mgmt \
+    obmc-phosphor-fan-mgmt \
+    obmc-phosphor-flash-mgmt \
+    obmc-host-state-mgmt \
+    obmc-chassis-state-mgmt \
+    obmc-bmc-state-mgmt \
+    "
+
+VIRTUAL-RUNTIME_obmc-host-state-manager ?= "x86-power-control"
+VIRTUAL-RUNTIME_obmc-chassis-state-manager ?= "x86-power-control"
+
+# HPE packagegroup providers
+PREFERRED_PROVIDER_virtual/obmc-chassis-mgmt = "packagegroup-hpe-apps"
+PREFERRED_PROVIDER_virtual/obmc-fan-mgmt = "packagegroup-hpe-apps"
+PREFERRED_PROVIDER_virtual/obmc-flash-mgmt = "packagegroup-hpe-apps"
 PREFERRED_PROVIDER_virtual/obmc-system-mgmt = "packagegroup-hpe-apps"

commit d81779c26b0d931a9984d341ebf05d04b37fc2d7

meta-hpe: power-control: enable button passthrough
Enable the button passthrough feature in x86-power-control which is
required to make the physiccal power- and reset-button functional.
The buttons are wired towards the CPLD, but it doesn't pass-through the
signal to power-ctrl-out by itself.

The button passthrough does not enable the UID button, but only the
power-related buttons.

Tested: Physically pressed the power- and reset button on a DL320 Gen11
        with both long- and short-press.

Fixes: 880fc06 ("meta-hpe: add host power management with
                 x86-power-control")
Signed-off-by: Tan Siewert <tan.siewert@9elements.com>

meta-canopy/meta-hpe/meta-common/recipes-x86/chassis/x86-power-control_%.bbappend
@@ -4,6 +4,10 @@ SRC_URI += " \
 	file://power-config-host0.json \
 	"
 
+EXTRA_OEMESON:append = " \
+    -Dbutton-passthrough=enabled \
+    "
+
 do_install:append() {
 	install -d ${D}/${datadir}/${PN}
 	install -m 0644 ${UNPACKDIR}/power-config-host0.json ${D}/${datadir}/${PN}